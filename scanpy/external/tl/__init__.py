585066d304ef03eeec438fcfbc1a22c2222d7acb
haidyi <haidyi@cs.unc.edu>
2023-10-20 21:51:32 -0400

    add cell sketching methods

--- scanpy/external/tl/__init__.py
+++ scanpy/external/tl/__init__.py
@@ -1,8 +1,9 @@
-from ._pypairs import cyclone, sandbag
+from ._harmony_timeseries import harmony_timeseries
+from ._palantir import palantir, palantir_results
 from ._phate import phate
 from ._phenograph import phenograph
-from ._palantir import palantir, palantir_results
-from ._trimap import trimap
-from ._harmony_timeseries import harmony_timeseries
+from ._pypairs import cyclone, sandbag
 from ._sam import sam
+from ._sketch import sketch
+from ._trimap import trimap
 from ._wishbone import wishbone
